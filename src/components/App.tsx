d84780e72e0a7da98e12ece1f244aaf61f9631a2
John Collins <johnscollins98@gmail.com>
2020-07-24 19:31:23 +0100

    add test reading from data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import './App.css';
+import data from '../assets/data/data.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {data.wing1.encounters.vale_guardian.label}
         </p>
       </header>
     </div>
