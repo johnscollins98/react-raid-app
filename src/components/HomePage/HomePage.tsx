4fb1f693d7a32048c79737ede83e03a047134daa
John Collins <johnscollins98@gmail.com>
2020-07-25 15:16:42 +0100

    initial version

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
 
+import './HomePage.css';
 import { IHomeProps } from "../../utilities/Interfaces";
 
 function HomePage(props: IHomeProps) {
+  const alertMessage = (message: string) => {
+    if (message) {
+      return <Alert variant="danger">{message}</Alert>
+    }
+  }
+
+  const logoGif = require("../../assets/images/SO_Logo.gif")
+
   return (
-    <div className="homepage">
-      <h1>Sunspear Order</h1>
-      <p>{props.errorMessage}</p>
-    </div>
+    <Container fluid>
+      <Row>
+        <img src={logoGif} width="100px" height="100px" className="logo" alt="logo" />
+      </Row>
+      <Row>
+        <Col>
+          <h1>Sunspear Order</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {alertMessage(props.errorMessage)}
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
