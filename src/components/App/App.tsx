958e3a0fd547eba9b9dd22e16cbe982ba749d775
John Collins <johnscollins98@gmail.com>
2020-07-25 02:15:10 +0100

    test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,7 +9,7 @@ import './App.css';
 import data from '../../assets/data/data.json';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { IWing } from '../../utilities/Interfaces';
-
+//test
 import Nav from '../Nav/Nav';
 import EncounterRouter from '../EncounterRouter';
 import HomePage from '../HomePage/HomePage';
