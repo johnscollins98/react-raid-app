193a4e24cc1855de0e63988f4d73f2e79cc53ead
John Collins <johnscollins98@gmail.com>
2020-07-25 13:43:56 +0100

    initial changes on new branch

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import { IHomeProps } from "../../utilities/Interfaces";
 
 function HomePage(props: IHomeProps) {
-  return <p>{props.errorMessage}</p>
+  return (
+    <div className="homepage">
+      <h1>Sunspear Order</h1>
+      <p>{props.errorMessage}</p>
+    </div>
+  )
 }
 
 export default HomePage;
