c4641dfb5022d07cb94e98ee3506c535bd050db1
Naylepsh <naylepsh@gmail.com>
2020-08-05 19:03:54 +0200

    add the rest of w4 notes

---
+++ src/components/EncounterNotes/DeimosNotes.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import RoleEmbed from "./../RoleEmbed/RoleEmbed";
+import GW2Skill from "./../GW2Skill/GW2Skill";
+
+function DeimosNotes() {
+  return (
+    <div>
+      <h3>General</h3>
+      <ul>
+        <li>
+          <RoleEmbed role="Heal" profession="firebrand" /> tanks
+        </li>
+        <li>
+          Banana "black kite" (only necessary during <GW2Skill id="37613" />
+          ).
+        </li>
+        <li>
+          Everyone except <RoleEmbed role="Heal" profession="firebrand" /> and{" "}
+          <RoleEmbed role="Hand Kite" profession="herald" /> goes down for
+          greens. For last add (assuming we don't sacrifice) save all CCs until
+          we're back above ground.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default DeimosNotes;
--- src/utilities/NoteMap.ts
+++ src/utilities/NoteMap.ts
@@ -6,16 +6,24 @@ import MatthiasNotes from "../components/EncounterNotes/MatthiasNotes";
 import EscortNotes from "../components/EncounterNotes/EscortNotes";
 import KeepConstructNotes from "../components/EncounterNotes/KeepConstructNotes";
 import TwistedCastleNotes from "../components/EncounterNotes/TwistedCastle";
+import MursaatOverseerNotes from "../components/EncounterNotes/MursaatOverseerNotes";
+import SamarogNotes from "./../components/EncounterNotes/SamarogNotes";
+import DeimosNotes from "../components/EncounterNotes/DeimosNotes";
 
 const NoteMap: Map<string, JSX.Element> = new Map<string, JSX.Element>();
 
 NoteMap.set("vale_guardian", ValeGuardianNotes());
 NoteMap.set("gorseval", GorsevalNotes());
+// TODO: Sabetha
 NoteMap.set("slothasor", SlothasorNotes());
 NoteMap.set("trio", BanditTrioNotes());
 NoteMap.set("matthias", MatthiasNotes());
 NoteMap.set("escort", EscortNotes());
 NoteMap.set("keep-construct", KeepConstructNotes());
 NoteMap.set("twisted-castle", TwistedCastleNotes());
+// TODO: Xera
+NoteMap.set("mursaat-overseer", MursaatOverseerNotes());
+NoteMap.set("samarog", SamarogNotes());
+NoteMap.set("deimos", DeimosNotes());
 
 export default NoteMap;
