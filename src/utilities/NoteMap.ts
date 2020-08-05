6c869a75a10d2b8942803f07a0b8f4d0b8ffd170
Naylepsh <naylepsh@gmail.com>
2020-08-05 17:50:21 +0200

    add trio notes

--- src/utilities/NoteMap.ts
+++ src/utilities/NoteMap.ts
@@ -1,11 +1,13 @@
 import ValeGuardianNotes from "../components/EncounterNotes/ValeGuardianNotes";
 import GorsevalNotes from "../components/EncounterNotes/GorsevalNotes";
 import SlothasorNotes from "../components/EncounterNotes/SlothasorNotes";
+import BanditTrioNotes from "../components/EncounterNotes/BanditTrio";
 
 const NoteMap: Map<string, JSX.Element> = new Map<string, JSX.Element>();
 
 NoteMap.set("vale_guardian", ValeGuardianNotes());
 NoteMap.set("gorseval", GorsevalNotes());
 NoteMap.set("slothasor", SlothasorNotes());
+NoteMap.set("trio", BanditTrioNotes());
 
 export default NoteMap;
