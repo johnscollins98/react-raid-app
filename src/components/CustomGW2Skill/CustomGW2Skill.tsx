92a3b9bc644231bffd7de774c610e26e11ce6dd6
John Collins <johnscollins98@gmail.com>
2020-08-07 15:56:30 +0100

    Add missing custom embed files

---
+++ src/components/CustomGW2Skill/CustomGW2Skill.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { TooltipTrigger } from 'armory-component-ui';
+
+import { ICustomGW2SkillProps } from "../../utilities/Interfaces";
+
+function CustomGW2Skill(props: ICustomGW2SkillProps) {
+  const img = require(`../../assets/images/skill-icons/${props.id}.png`);
+  return (
+    <TooltipTrigger
+      data={{
+        name: props.label,
+        description: "Custom embed for broken API."
+      }}
+      type="skill"
+    >
+      <span>
+
+        <img height="25" width="25" src={img} />{" "}
+        <a href={props.wikiLink}>{props.label}</a>
+      </span>
+    </TooltipTrigger>
+  )
+}
+
+export default CustomGW2Skill;
