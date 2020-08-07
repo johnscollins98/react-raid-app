b19080140d2c588fda849efa554b8b35f20f92f9
John Collins <johnscollins98@gmail.com>
2020-08-08 00:36:35 +0100

    Update KC notes and composition

--- src/components/EncounterNotes/KeepConstructNotes.tsx
+++ src/components/EncounterNotes/KeepConstructNotes.tsx
@@ -11,28 +11,48 @@ function KeepConstructNotes() {
         <li>
           <RoleEmbed role="Heal" profession="druid" /> tanks.
         </li>
+      </ul>
+      <h3>Orb Phase</h3>
+      <ul>
+        <li>
+          <RoleEmbed role="Heal" profession="druid" /> pushes 2 orbs. (1 for
+          first phase is usually fine)
+        </li>
+        <li>
+          <RoleEmbed role="Pwr Boon" profession="chronomancer" /> help the{" "}
+          <RoleEmbed role="Heal" profession="druid" /> with orbs using{" "}
+          <GW2Skill id="10186" />.
+        </li>
+        <li>
+          Everyone be ready with boons in time for bursting Keep Construct.
+        </li>
+      </ul>
+      <h3>Surviving while Druid pushes</h3>
+      <ul>
         <li>
           <RoleEmbed role="Cnd Boon" profession="firebrand" /> should help out
           with heals with <GW2Skill id="41780" /> during orb/collecting phases.
         </li>
         <li>
-          <RoleEmbed role="Pwr" profession="holosmith" /> can take{" "}
-          <GW2Skill id="5857" /> if more healing is needed.
+          <RoleEmbed role="Pwr" profession="dragonhunter" /> can use{" "}
+          <GW2Skill id="30783" /> to help also.
         </li>
         <li>
-          Attack KC during the collecting phase to heal from{" "}
+          <RoleEmbed role="Pwr" profession="holosmith" /> can take{" "}
+          <GW2Skill id="5857" /> if more healing is <strong>needed</strong>.
+        </li>
+        <li>Use your own heal skills!</li>
+      </ul>
+      <h3>Collection Phase</h3>
+      <ul>
+        <li>
+          Attack Keep Construct during the collecting phase to heal from{" "}
           <GW2Skill id="21773" />.
         </li>
         <li>
-          If during the collecting phase you're assigned red color, DO NOT TAKE
-          WHITE ORBS (and vice versa).
+          <strong>Only collect orbs with the same color as you!</strong>
         </li>
-        <li>
-          We're doing 2 orbs.{" "}
-          <RoleEmbed role="Pwr Boon" profession="chronomancer" /> help the{" "}
-          <RoleEmbed role="Heal" profession="druid" /> with pushing.
-        </li>
-        <li>Be ready with boons in time for bursting KC.</li>
+
         <li>
           DPS and stack tightly after collecting phase (during{" "}
           <GW2Skill id="34971" />) to get healing from <GW2Skill id="31677" />.
