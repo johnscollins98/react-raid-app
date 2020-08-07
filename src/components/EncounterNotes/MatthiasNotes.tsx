a7b06e6a2e269fd0a44bbdce405ea8c5f8a45c8f
John Collins <johnscollins98@gmail.com>
2020-08-08 00:26:52 +0100

    Update notes for Matthias

--- src/components/EncounterNotes/MatthiasNotes.tsx
+++ src/components/EncounterNotes/MatthiasNotes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import RoleEmbed from "../RoleEmbed/RoleEmbed";
 import GW2Skill from "../GW2Skill/GW2Skill";
+import CustomGW2Skill from "../CustomGW2Skill/CustomGW2Skill";
 
 function MatthiasNotes() {
   return (
@@ -9,8 +10,14 @@ function MatthiasNotes() {
       <h3>Individual Jobs</h3>
       <ul>
         <li>
-          <RoleEmbed role="Cnd" profession="mirage" /> brings{" "}
-          <GW2Skill id="10302" />.
+          <RoleEmbed role="Cnd" profession="firebrand" />s alternate using{" "}
+          <CustomGW2Skill
+            id="valiant-bulwark"
+            label="Chapter 3: Valiant Bulwark"
+            wikiLink="https://wiki.guildwars2.com/wiki/Chapter_3:_Valiant_Bulwark"
+          />{" "}
+          in <GW2Skill id="42259" /> to reflect <GW2Skill id="34480" /> and
+          break his shield.
         </li>
         <li>
           <RoleEmbed role="Pwr Boon" profession="daredevil" /> /{" "}
