c3c9bde69dd7087aa204995bcdea6303fcba9f58
John Collins <johnscollins98@gmail.com>
2020-08-08 00:55:50 +0100

    Update SH composition and notes

--- src/components/EncounterNotes/SoullessHorrorNotes.tsx
+++ src/components/EncounterNotes/SoullessHorrorNotes.tsx
@@ -7,12 +7,16 @@ import GW2Trait from "../GW2Trait/GW2Trait";
 function SoullessHorrorNotes() {
   return (
     <div>
-      <h3>General</h3>
+      <h3>Tanking</h3>
       <ul>
         <li>
-          <RoleEmbed role="Boon" profession="chronomancer" /> and{" "}
+          <RoleEmbed role="Pwr Boon" profession="chronomancer" /> and{" "}
           <RoleEmbed role="Heal" profession="firebrand" /> tank.
         </li>
+        <li>Tanks, don't be mutes, state clearly where you intend to move</li>
+      </ul>
+      <h3>Golems</h3>
+      <ul>
         <li>
           <RoleEmbed role="Heal" profession="druid" /> should take{" "}
           <GW2Trait id="1698" /> in Marksmanship for pushing.
@@ -20,11 +24,14 @@ function SoullessHorrorNotes() {
         <li>
           Don't CC the golem or the druid <strong>will</strong> yell at you
         </li>
+      </ul>
+      <h3>CC Phase</h3>
+      <ul>
         <li>Be ready for the first CC at 66%.</li>
         <li>
           After 66%, CC comes up after every 2nd <GW2Skill id="48500" />.
         </li>
-        <li>Tanks, don't be mutes, state clearly where you intend to move</li>
+        <li>Remember that when the CC phase triggers all walls despawn.</li>
       </ul>
     </div>
   );
